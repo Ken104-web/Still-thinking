8bbb8b0c0b8ec4cbbde4b09d872e6f0fd2e4d399
KenCorp <kennethwarui35@gmail.com>
2023-10-03 14:16:34 +0300

    created models

---
+++ server/myapp/models.py
@@ -0,0 +1,35 @@
+from myapp import db
+
+user_site_table = db.Table('user_site_association',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('site_id', db.Integer, db.ForeignKey('sites.id'))
+)
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+
+    reviews = db.relationship('Review', backref='user')
+
+
+class TouristAttrationSite(db.Model):
+    __tablename__ = 'sites'
+
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String)
+    description = db.Column(db.String)
+    rating = db.Column(db.Integer)
+
+    reviews = db.relationship('Review', backref='site')
+class Review(db.Model):
+    tablename = "reviews"
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.String)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    touristattrationsite_id = db.Column(db.Integer, db.ForeignKey('sites.id'))
+
+    User.sites = db.relationship('TouristAttractionSite',secondary=user_site_table, backref=db.backref('users'))
+
---
+++ server/run.py
@@ -0,0 +1,4 @@
+from myapp import db, app, models
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
